60283d67d80ba86fc23f0d9b1acddb5fbf75977a
thaker0503 <thaker.yatharth.05@gmail.com>
2025-08-18 07:38:07 +0000

    add configuration files for app links and asset links

---
+++ next.config.js
@@ -0,0 +1,29 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async headers() {
+    return [
+      // iOS: force correct content-type
+      {
+        source: "/.well-known/apple-app-site-association",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "application/json",
+          },
+        ],
+      },
+      // Android: assetlinks.json also needs correct content-type
+      {
+        source: "/.well-known/assetlinks.json",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "application/json",
+          },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
